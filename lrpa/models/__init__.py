f2418769366114559aeb3aa14ee78f5a9e98bf6d
Xiaoqian Liu <simon.liuxq@gmail.com>
2026-04-22 15:25:49 +0200

    Update __init__.py

--- lrpa/models/__init__.py
+++ lrpa/models/__init__.py
@@ -1,4 +1,5 @@
 from lrpa.models.bernal_bilayer import BernalBilayer
+from lrpa.models.rhombohedral_multilayer import RhombohedralMultilayer
 from lrpa.soc_parameters import SOCParameters
 
-__all__ = ["BernalBilayer", "SOCParameters"]
+__all__ = ["BernalBilayer", "RhombohedralMultilayer", "SOCParameters"]
